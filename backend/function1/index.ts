d07d724c3133013c71fd2dbd2f7ff4913965c8e6
lderdzyan <lderdzyan@vtgsoftware.com>
2026-01-26 11:20:10 +0400

    Backend

--- backend/function1/index.ts
+++ backend/function1/index.ts
@@ -1,7 +1,7 @@
 export const handler = async () => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!666622223333333'),
+    body: JSON.stringify('Hello from Lfffffambda!666622223333333'),
   };
   return response;
 };
