05b598ea4e485eefd486bd663fc835b8ae2a7776
loosita <Lusderdzyan77@gmail.com>
2026-01-29 22:38:49 +0400

    Ls test2

--- backend/function2/index.ts
+++ backend/function2/index.ts
@@ -1,7 +1,7 @@
 export const handler = async () => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hi hi'),
+    body: JSON.stringify('Hi hi hi'),
   };
   return response;
 };
