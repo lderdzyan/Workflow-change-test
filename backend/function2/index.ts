32343f7dbd994d4d0c1aadd24f7c57c93cda4fe1
lderdzyan <lderdzyan@vtgsoftware.com>
2026-01-30 13:16:18 +0400

    Build & Deploy

--- backend/function2/index.ts
+++ backend/function2/index.ts
@@ -1,7 +1,7 @@
 export const handler = async () => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hi hi hi hihi nothing changed? NO'),
+    body: JSON.stringify('Hi hi hi hihi nothing changed? yeS'),
   };
   return response;
 };
